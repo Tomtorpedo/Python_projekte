4aa96b3aa83de37ac14df326aa298e8e08bd9e63
Tomtorpedo <vervoort.tom@proton.me>
2024-10-16 19:00:30 +0200

    modified:   dunkelwald.py

--- dunkelwald.py
+++ dunkelwald.py
@@ -1,7 +1,23 @@
 #   ^^<>>>v>v<<<^>v<<vv>>>>^^>^^>vv
-rückpfad = "bla"
+#aufgabe 1-3
+"""
+chars=[]
+#   string=input()  das kann genutzt werden, um etwas einzugeben
+string="^^<>>>v>v<<<^>v<<vv>>>>^^>^^>vv"
+for i in string:
+    chars.append(i) #wandelt einen string in eine liste mit einzelnen charaktern
+
+pfad=chars
+
+rückpfad=[]
 def rückweg_aus_wald(pfad):
-    rückpfad = pfad[::-1]
-    return rückpfad
-print(rückweg_aus_wald("hallo"))
-# o notation ist o=n
+    pfad.reverse()          #die .reverse methode für listen wird verwendet
+    return pfad
+rückpfad=rückweg_aus_wald(pfad)
+
+output="".join(rückpfad)        # die liste wird zu einem string zurück geformt
+print(output)
+"""
+#aufgabe 5
+#wenn man die anweisungen als vektoren aufschreibt sieht man, dass man häufiger am selben punkt borbei kommt, man könnte also abkürzen
+
