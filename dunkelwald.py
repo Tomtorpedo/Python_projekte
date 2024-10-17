65a3b75ed656ffd8e46410481d4b7d69b26b3882
tomtorpedo <vervoort.tom@proton.me>
2024-10-17 10:37:50 +0200

    modified:   dunkelwald.py

--- dunkelwald.py
+++ dunkelwald.py
@@ -22,44 +22,47 @@ print(output)
 #wenn man die anweisungen als vektoren aufschreibt sieht man, dass man häufiger am selben punkt borbei kommt, man könnte also abkürzen
 
 #aufgabe 6
-chars=[]
-#   string=input()  das kann genutzt werden, um etwas einzugeben
-string="^^<>>>v>v<<<^>v<<vv>>>>^^>^^>vv"
-for i in string:
-    chars.append(i) #wandelt einen string in eine liste mit einzelnen charaktern
-vektoren_x=[]
-vektoren_y=[]
-pfad=chars
-for i in pfad:                  #für jeden wert wird ein vektor erstellt
-    if i=="^":
-        vektoren_x.append(1)
-        vektoren_y.append(0)
-    elif i==">":
-        vektoren_x.append(0)
-        vektoren_y.append(1)
-    elif i=="v":
-        vektoren_x.append(-1)
-        vektoren_y.append(0)
-    elif i=="<":
-        vektoren_x.append(0)
-        vektoren_y.append(-1)
+"""
+string=input()  #das kann genutzt werden, um etwas einzugeben
+def geht_schneller_zurück(string):
+    chars=[]
+    #string="^^<>>>v>v<<<^>v<<vv>>>>^^>^^>vv"
+    for i in string:
+        chars.append(i) #wandelt einen string in eine liste mit einzelnen charaktern
+    vektoren_x=[]
+    vektoren_y=[]
+    pfad=chars
+    for i in pfad:                  #für jeden wert wird ein vektor erstellt
+        if i=="^":
+            vektoren_x.append(1)
+            vektoren_y.append(0)
+        elif i==">":
+            vektoren_x.append(0)
+            vektoren_y.append(1)
+        elif i=="v":
+            vektoren_x.append(-1)
+            vektoren_y.append(0)
+        elif i=="<":
+            vektoren_x.append(0)
+            vektoren_y.append(-1)
 
-koordinaten_x=[0]               #das koordinatensystem wird aufgestellt
-koordinaten_y=[0]
-rx=0
-ry=0
-is_shortest_path=True
-for i in vektoren_x:
-    rx=rx+i
-    koordinaten_x.append(rx)     # die vektoren werden eingepflegt >> es entsteht eine "karte"
-for i in vektoren_y:
-    ry=ry+i
-    koordinaten_y.append(ry)
+    koordinaten_x=[0]               #das koordinatensystem wird aufgestellt
+    koordinaten_y=[0]
+    rx=0
+    ry=0
+    is_shortest_path=True
+    for i in vektoren_x:
+        rx=rx+i
+        koordinaten_x.append(rx)     # die vektoren werden eingepflegt >> es entsteht eine "karte"
+    for i in vektoren_y:
+        ry=ry+i
+        koordinaten_y.append(ry)
 
 
-for i in range(len(chars)+1):
-    if koordinaten_x.count(koordinaten_x[i])>1 and koordinaten_y.count(koordinaten_y[i])>1 
-        #print("sie sind schonmal an den koordinaten", koordinaten_x[i],"|",koordinaten_y[i],"gewesen.")
-        is_shortest_path=False
-if is_shortest_path==False:
-    print("es gibt einen kürzeren weg!")
+    for i in range(len(chars)+1):
+        if koordinaten_x.count(koordinaten_x[i])>1 and koordinaten_y.count(koordinaten_y[i])>1:
+            #print("sie sind schonmal an den koordinaten", koordinaten_x[i],"|",koordinaten_y[i],"gewesen.")
+            is_shortest_path=False
+    return(not is_shortest_path)
+print(geht_schneller_zurück(string))
+"""
