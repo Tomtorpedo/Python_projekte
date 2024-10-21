fa7ff98dcbd0513763f11a9836a3f87f7b229ccf
tomtorpedo <vervoort.tom@proton.me>
2024-10-21 12:50:10 +0200

    modified:   dunkelwald.py

--- dunkelwald.py
+++ dunkelwald.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 #   ^^<>>>v>v<<<^>v<<vv>>>>^^>^^>vv
 #aufgabe 1-3
 """
@@ -22,45 +24,77 @@ print(output)
 #wenn man die anweisungen als vektoren aufschreibt sieht man, dass man häufiger am selben punkt borbei kommt, man könnte also abkürzen
 
 #aufgabe 6
-"""
-string=input()  #das kann genutzt werden, um etwas einzugeben
+string="v>v^>>vv^><>^v>"
+#string=input()
+
+  #das kann genutzt werden, um etwas einzugeben
 def geht_schneller_zurück(string):
     chars=[]
-    #string="^^<>>>v>v<<<^>v<<vv>>>>^^>^^>vv"
     for i in string:
         chars.append(i) #wandelt einen string in eine liste mit einzelnen charaktern
-    vektoren_x=[]
-    vektoren_y=[]
+    vektoren=[]
     pfad=chars
     for i in pfad:                  #für jeden wert wird ein vektor erstellt
         if i=="^":
-            vektoren_x.append(1)
-            vektoren_y.append(0)
+            vektoren.append((0,1))
         elif i==">":
-            vektoren_x.append(0)
-            vektoren_y.append(1)
+            vektoren.append((1,0))
         elif i=="v":
-            vektoren_x.append(-1)
-            vektoren_y.append(0)
+            vektoren.append((0,-1))
         elif i=="<":
-            vektoren_x.append(0)
-            vektoren_y.append(-1)
+            vektoren.append((-1,0))
+    koordinaten=[(0,0)]
+    kx = 0
+    ky= 0
+    for i in vektoren:
+        kx += i[0]
+        ky += i[1]
+        koordinaten.append((kx,ky))
 
-    koordinaten_x=[0]               #das koordinatensystem wird aufgestellt
-    koordinaten_y=[0]
-    rx=0
-    ry=0
-    is_shortest_path=True
-    for i in vektoren_x:
-        rx=rx+i
-        koordinaten_x.append(rx)     # die vektoren werden eingepflegt >> es entsteht eine "karte"
-    for i in vektoren_y:
-        ry=ry+i
-        koordinaten_y.append(ry)
+    
+    #hier wird nun gezählt, wie oft und an welcher stelle eine koordinate auftaucht
+    #alles was sich zwischen zwei koordinaten befindet, die zwei mal auftauchen, wird gelöscht, da es sich dann um einen umweg handelt
+    r=0
+    besuchte_koordinaten=[]
+    for i in koordinaten:
+        if i in besuchte_koordinaten:
+            punkt_ab_doppelung=besuchte_koordinaten.index(i)
+            del besuchte_koordinaten[(punkt_ab_doppelung+1):(r+1)]
+        else:
+            besuchte_koordinaten.append(i)
+        r+=1
+    print(besuchte_koordinaten)
+    print(koordinaten)
+    x, y = zip(*besuchte_koordinaten)
+    plt.scatter(x,y)
+    plt.plot(0,0, "ro")
+    plt.plot(kx,ky, "go")
+    plt.show()
+    x, y = zip(*koordinaten)
+    plt.scatter(x,y)
+    plt.plot(0,0, "ro")
+    plt.plot(kx,ky, "go")
+    plt.show()
+geht_schneller_zurück(string)
 
 
+"""
+    x, y = zip(*koordinaten)
+    plt.scatter(x,y)
+    plt.plot(0,0, "ro")
+    plt.plot(kx,ky,"go")
+    print(koordinaten)
+    plt.show()
+"""
+
+        
+
+       
+
+
+"""
     for i in range(len(chars)+1):
-        if koordinaten_x.count(koordinaten_x[i])>1 and koordinaten_y.count(koordinaten_y[i])>1:
+        if koordinaten.count(koordinaten_x[i])>1 and koordinaten_y.count(koordinaten_y[i])>1:
             #print("sie sind schonmal an den koordinaten", koordinaten_x[i],"|",koordinaten_y[i],"gewesen.")
             is_shortest_path=False
     return(not is_shortest_path)
