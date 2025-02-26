2d621ac143fb902648ee903df77d8e56dc28d250
tomtorpedo <vervoort.tom@proton.me>
2025-02-26 09:29:28 +0100

    modified:   .gitignore 	modified:   merge_sort.py 	modified:   toolbox.py

--- merge_sort.py
+++ merge_sort.py
@@ -2,27 +2,34 @@ def merge(a:list, b:list)->list:
     """
     Zwei sortierte listen werden sortiert vereinigt."""
     
-    #variablen werden initialisiert
     c=[]
-    ia=0 #iterator a
-    ib=0 #iterator b
+    ia=0 
+    ib=0 
     
-    while ia < len(a):
+    while ia < len(a) and ib < len(b):
         if ib<len(b) and a[ia]<b[ib]:   #wenn b noch nicht durch gelaufen ist und a kleiner als b ist wird a in c eingefügt
             c.append(a[ia])
             ia+=1
-        elif ib<len(b):                #sonst wenn b noch nicht durch gelaufen ist, b also größer als a ist, wird b in c eingefügt
+        else:                #sonst wenn b noch nicht durch gelaufen ist, b also größer als a ist, wird b in c eingefügt
             c.append(b[ib])
-            ib+=1
-        else:                        #wenn b durch gelaufen ist, werden die restlichen einträge von a in c eingefügt
-            c.append(a[ia])
-            ia+=1
+            ib+=1                       
+    if ia<len(a):                   #wenn b durch gelaufen ist, werden die restlichen einträge von a in c eingefügt
+        c.extend(a[ia:])            
     if ib<len(b):                   #wenn a durch gelaufen ist, werden die restlichen einträge von b in c eingefügt
         c.extend(b[ib:])
     return c
 
 
-a=[1,3,4,7,19]
-b=[2,3,5,6,10,21]
+def merge_sort(liste:list)->list:
+    """sortiert eine liste"""
+    if len(liste)==1:
+        return liste
+    if len(liste)>1:
+        liste_rechts=liste[(len(liste)//2):]
+        liste_links=liste[:(len(liste)//2)]
+        liste_rechts=merge_sort(liste_rechts)
+        liste_links=merge_sort(liste_links)
+        liste_neu=merge(liste_links,liste_rechts)
+        return liste_neu
 
-print(merge(a,b))
+
--- toolbox.py
+++ toolbox.py
@@ -1,9 +1,19 @@
-import time
 if __name__=="__main__":
     print("test")
 
-def timer(funktion,input)->int:
+def timer(funktion,*input)->int:
+    """Stoppt die Zeit einer Funktion"""
+    import time
     start=time.perf_counter_ns()
-    funktion(input)
+    funktion(*input)
     stop=time.perf_counter_ns()
     return(stop-start)
+
+def list_gen(len:int)->list:
+    """Generiert unsortierte liste mit [len] Einträgen"""
+    import random
+    list=[]
+    for i in range(len):
+        zahl=random.randint(0,999)
+        list.append(zahl)
+    return list
