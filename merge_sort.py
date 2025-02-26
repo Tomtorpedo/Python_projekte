547c065d1a6c8dd9c0241f5c00ba52d33e92adaa
tomtorpedo <vervoort.tom@proton.me>
2025-02-26 07:54:01 +0100

    modified:   merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -8,16 +8,16 @@ def merge(a:list, b:list)->list:
     ib=0 #iterator b
     
     while ia < len(a):
-        if ib<len(b) and a[ia]<b[ib]:   #wenn b noch nicht durch ist und a kleiner als b ist wird a in c eingefügt
+        if ib<len(b) and a[ia]<b[ib]:   #wenn b noch nicht durch gelaufen ist und a kleiner als b ist wird a in c eingefügt
             c.append(a[ia])
             ia+=1
-        elif ib<len(b):                #sonst wenn b noch nicht durch ist, b also größer als a ist, wird b in c eingefügt
+        elif ib<len(b):                #sonst wenn b noch nicht durch gelaufen ist, b also größer als a ist, wird b in c eingefügt
             c.append(b[ib])
             ib+=1
-        else:                        #wenn b durch ist, werden die restlichen einträge von a in c eingefügt
+        else:                        #wenn b durch gelaufen ist, werden die restlichen einträge von a in c eingefügt
             c.append(a[ia])
             ia+=1
-    if ib<len(b):                   #wenn a durch ist, werden die restlichen einträge von b in c eingefügt
+    if ib<len(b):                   #wenn a durch gelaufen ist, werden die restlichen einträge von b in c eingefügt
         c.extend(b[ib:])
     return c
 
